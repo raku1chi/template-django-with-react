082cc0b1016380860584f55833c6a6f6aec4ff39
tsht0 <25150296+tsht0@users.noreply.github.com>
2025-06-12 01:08:19 +0900

    chore: Dockerfileと設定ファイルを更新し、ビルドプロセスを最適化

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -26,9 +26,7 @@ SECRET_KEY = (os.getenv("SECRET_KEY"),)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.getenv("DEBUG") or False)
 
-ALLOWED_HOSTS = []
-if hosts := os.getenv("ALLOWED_HOSTS"):
-    ALLOWED_HOSTS = hosts.split(",")
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -127,6 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
